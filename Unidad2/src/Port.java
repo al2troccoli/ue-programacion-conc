1a4101300786efcb6d8c13846df350123b379dfa
Aldo Troccoli <aatroccoli@minsait.com>
2020-03-14 20:05:47 +0100

    Actividad Individual 2 - Javadoc

--- Unidad2/src/Port.java
+++ Unidad2/src/Port.java
@@ -3,12 +3,28 @@ import java.util.Queue;
 import java.util.concurrent.Semaphore;
 
 public class Port {
-
+    /**
+     * El buffer es la cola donde vamos a almacenar todos los mensajes. Funciona como un buzón.
+     */
     private static Queue<Integer> buffer = new LinkedList<Integer>();
+
+    /**
+     * Esta constanste establece el tamaño del buffer. En este caso se ha definido para un total de 30 mensajes.
+     */
     public static final int bufferSize = 30;
 
+    /**
+     * Este semáforo le permite saber al Receiver si hay mensajes para consumir en el buffer. Si no hay mensajes disponibles
+     * el hilo del Receiver se va a bloquear en este semáforo hasta que el Transmitter cree un nuevo mensaje y lo
+     * deposite en el buffer.
+     */
     public static Semaphore semMessagesAvailable = new Semaphore(0, true);
 
+    /**
+     * Este semáforo le permite saber al Transmitter si el buffer se encuentra lleno o si aun tiene capacidad para
+     * seguir colocando nuevos mensajes. Si el buffer está lleno el hilo del Transmitter se bloquea en este semáforo
+     * hasta que el Receiver consuma un mensaje y lo saque del buffer.
+     */
     public static Semaphore semBufferAvailable = new Semaphore(bufferSize, true);
 
     /**
