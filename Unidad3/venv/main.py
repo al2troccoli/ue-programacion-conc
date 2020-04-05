53b1763cdda4d19c1551274a542ac9ab362848ce
Aldo Troccoli <aatroccoli@minsait.com>
2020-04-05 20:23:55 +0200

    Actividad individual 3 - Documentando el código

--- Unidad3/venv/main.py
+++ Unidad3/venv/main.py
@@ -1,74 +1,111 @@
+# coding=utf-8
 import math
 import multiprocessing
 import random
 import sys
 import time
 
-
+# Este es el alogritmo que hace la mezcla de los subarrays ordenados
 def merge(*args):
-    # Support explicit left/right args, as well as a two-item
-    # tuple which works more cleanly with multiprocessing.
-    left, right = args[0] if len(args) == 1 else args
-    left_length, right_length = len(left), len(right)
-    left_index, right_index = 0, 0
-    merged = []
-    while left_index < left_length and right_index < right_length:
-        if left[left_index] <= right[right_index]:
-            merged.append(left[left_index])
-            left_index += 1
+    # Se utiliza argumentos variables porque en modo secuencial
+    # se pasan de manera explícita dos argumentos: mitad_izquierda y mitad_derecha
+    # En modo paralelo se pasa un único argumento que es una tupla de dos
+    # elementos con la forma: ([],[])
+    izquierda, derecha = args[0] if len(args) == 1 else args
+
+    longitud_izq, longitud_derecha = len(izquierda), len(derecha)
+    indice_izq, indice_derecho = 0, 0
+    resultado = []
+    while indice_izq < longitud_izq and indice_derecho < longitud_derecha:
+        if izquierda[indice_izq] <= derecha[indice_derecho]:
+            resultado.append(izquierda[indice_izq])
+            indice_izq += 1
         else:
-            merged.append(right[right_index])
-            right_index += 1
-    if left_index == left_length:
-        merged.extend(right[right_index:])
+            resultado.append(derecha[indice_derecho])
+            indice_derecho += 1
+    if indice_izq == longitud_izq:
+        resultado.extend(derecha[indice_derecho:])
     else:
-        merged.extend(left[left_index:])
-    return merged
+        resultado.extend(izquierda[indice_izq:])
+    return resultado
 
-
-def merge_sort_secuencial(data):
+# Este es el algoritmo merge sort divide and conquer.
+# Nada en especial que señalar en esta implementación
+def merge_sort(data):
     longitud = len(data)
     if longitud <= 1:
         return data
     medio = int(longitud / 2)
-    mitadIzquierda = merge_sort_secuencial(data[:medio])
-    mitadDerecha = merge_sort_secuencial(data[medio:])
-    return merge(mitadIzquierda, mitadDerecha)
+    mitad_izquierda = merge_sort(data[:medio])
+    mitad_derecha = merge_sort(data[medio:])
+    return merge(mitad_izquierda, mitad_derecha)
 
-
+# Este es el algoritmo merge sort modificado para ejecutarse de manera paralela.
 def merge_sort_paralelo(data):
-    # Creates a pool of worker processes, one per CPU core.
-    # We then split the initial data into partitions, sized
-    # equally per worker, and perform a regular merge sort
-    # across each partition.
-    totalProcesos = multiprocessing.cpu_count()
-    print('totalProcesos', totalProcesos)
-    pool = multiprocessing.Pool(processes=totalProcesos)
-    longitudSubarray = int(math.ceil(float(len(data)) / totalProcesos))
-    data = [data[i * longitudSubarray:(i + 1) * longitudSubarray] for i in range(totalProcesos)]
-    data = pool.map(merge_sort_secuencial, data)
-    # Each partition is now sorted - we now just merge pairs of these
-    # together using the worker pool, until the partitions are reduced
-    # down to a single sorted result.
+    # Obtenemos la cantidad de cores que tiene el CPU
+    total_procesos = multiprocessing.cpu_count()
+
+    # Creamos un pool de workers processes, uno por core
+    pool = multiprocessing.Pool(processes=total_procesos)
+
+    # Obtenemos la longitud de los subarrays que se van a repartir a los workers.
+    # Esto es lo que se conoce como load balance
+    longitud_subarray = int(math.ceil(float(len(data)) / total_procesos))
+
+    # Sobrescribimos data con la lista de subarrays
+    data = [data[i * longitud_subarray:(i + 1) * longitud_subarray] for i in range(total_procesos)]
+
+    # Cada worker levanta un proceso donde ejecuta el merge sort al subarray correspondiente
+    # Cuando todos los workers terminan, los subarrays ya estarían ordenados
+    data = pool.map(merge_sort, data)
+
+    # Una vez los subarrays se encuentran ordenados ahora tenemos que mezclarlos de dos en dos
+    # con el objetivo de reducir todos los subarrays a un solo array ordenado
+
     while len(data) > 1:
         # If the number of partitions remaining is odd, we pop off the
         # last one and append it back after one iteration of this loop,
         # since we're only interested in pairs of partitions to merge.
+
+        # Si el número de subarrays es impar, nos guardamos el último para mezclarlo en
+        # próximas iteraciones del while
         extra = data.pop() if len(data) % 2 == 1 else None
+
+        # Se combinan los subarrays de dos en dos para obtener una estructura de tuplas
+        # como la siguiente: [(subarray1, subarray2), (subarray3,subarray4)...]
         data = [(data[i], data[i + 1]) for i in range(0, len(data), 2)]
+
+        # Ahora reutilizamos el pool creado para paralelizar el merge sort de las tuplas de subarrays
+        # Si data es impar añadimos al final el último subarray el cual no pudo formar parte de
+        # ninguna tupla
         data = pool.map(merge, data) + ([extra] if extra else [])
     return data[0]
 
 
 if __name__ == "__main__":
-    numeroExpediente = 1000 #21869286
-    arrayNoOrdenado = [random.randint(-numeroExpediente, numeroExpediente) for _ in range(numeroExpediente)]
-    print(arrayNoOrdenado)
-    print('=======================================')
-    for sort in merge_sort_secuencial, merge_sort_paralelo:
-        tiempoInicio = time.time()
-        arrayOrdenado = sort(arrayNoOrdenado)
-        tiempoFinal = time.time() - tiempoInicio
-        # print(sort.__name__, arrayOrdenado)
-        print(sort.__name__, tiempoFinal)
-        print('---------------------------------------')
+    # Imprimo en pantalla los cores del ordenador solo a modo informativo
+    total_procesos = multiprocessing.cpu_count()
+    print '======================================='
+    print 'Total de cores: ', total_procesos
+    print '======================================='
+    print
+
+    # Defino mi número de expediente con el cual vamos a generar los datos de manera aleatroria.
+    numero_expediente = 21869286
+    array_no_ordenado = [random.randint(-numero_expediente, numero_expediente) for _ in range(numero_expediente)]
+
+    # Mostramos los datos desordenados
+    print 'Array no ordenado'
+    print array_no_ordenado
+    print '======================================='
+    print
+
+    # Vamos a probar los dos algoritmos de merge sort que hemos hecho (secuencial y paralelo)
+    # Vamos a medir el tiempo que toma cada uno de estos para poder comparar su rendimiento.
+    for sort in merge_sort, merge_sort_paralelo:
+        tiempo_inicio = time.time()
+        array_ordenado = sort(array_no_ordenado)
+        tiempo_final = time.time() - tiempo_inicio
+        print sort.__name__, tiempo_final
+        print '======================================='
+        print
